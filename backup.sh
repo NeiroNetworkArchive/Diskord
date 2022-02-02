72804c27926d60705ac167582b0ea136fbed971a
Nerahikada <hkshirorom@yahoo.co.jp>
2022-02-02 22:07:26 +0900

    ファイルの名前処理関連を改善

--- backup.sh
+++ backup.sh
@@ -9,10 +9,11 @@ post_file () {
 
 if ! type 7zr > /dev/null 2>&1; then echo "7zr not found, install 'p7zip'"; exit 1; fi
 if [ -z $DISKORD_WEBHOOK ]; then echo "No webhook url has been set, run 'export DISKORD_WEBHOOK=<webhook url>'"; exit 1; fi
-if [ -z $1 ]; then echo "Source file is not specified"; exit 1; fi
-if [ ! -e $1 ]; then echo "file not found"; exit 1; fi
+if [ -z "$1" ]; then echo "Target file is not specified"; exit 1; fi
+file=`realpath -m "$1"`
+if [ ! -e "$file" ]; then echo "file not found"; exit 1; fi
 
-archive="`basename ${1%.*}` `date +"%Y-%m-%d_%H-%M-%S"`.7z"
+archive="`basename "$file"` `date +"%Y-%m-%d_%H-%M-%S"`.7z"
 7zr -v8191k -bso0 -bsp0 a "$archive" "$1"
 
 find * -type f -name "$archive.*" -print0 | while IFS= read -r -d '' file; do
