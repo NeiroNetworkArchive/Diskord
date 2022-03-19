df947bbe4ac448f4b0abf97e3e2fd0987ac3b932
Nerahikada <hkshirorom@yahoo.co.jp>
2022-03-19 21:29:17 +0900

    パスワードを指定して暗号化する機能を実装

--- backup.sh
+++ backup.sh
@@ -7,15 +7,19 @@ post_file () {
 		$DISKORD_WEBHOOK
 }
 
-if ! type 7zr > /dev/null 2>&1; then echo "7zr not found, install 'p7zip'"; exit 1; fi
+if ! type 7zr > /dev/null 2>&1; then echo "7z not found, install 'p7zip-full'"; exit 1; fi
 if [ -z $DISKORD_WEBHOOK ]; then echo "No webhook url has been set, run 'export DISKORD_WEBHOOK=<webhook url>'"; exit 1; fi
 if [ -z "$1" ]; then echo "Target file is not specified"; exit 1; fi
 file=`realpath -m "$1"`
 if [ ! -e "$file" ]; then echo "File '$file' was not found"; exit 1; fi
 
 cd "`dirname "$file"`"
-archive="`basename "$file"` `date +"%Y-%m-%d_%H-%M-%S"`.7z"
-7zr -v8191k -bso0 -bsp0 a "$archive" "`basename "$file"`"
+archive="`basename "$file"` `date +"%Y-%m-%d_%H-%M-%S"`"
+if [ -z "$2" ]; then
+	7z -v8191k -bso0 -bsp0 a "$archive" "`basename "$file"`"
+else
+	7z -mhe=on -p"$2" -v8191k -bso0 -bsp0 a "$archive" "`basename "$file"`"
+fi
 
 find * -type f -name "$archive.*" -print0 | while IFS= read -r -d '' part; do
 	post_file "$part"
